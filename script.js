5554d0bcd71999a1b66b2ce095ade89c56b03e32
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-05 01:32:29 +0530

    when player wins bg changed logic added.

--- script.js
+++ script.js
@@ -13,12 +13,19 @@ showSecretNumber.textContent = secretNumber;
 
 btnCheck.addEventListener("click", function () {
   const userInput = Number(document.querySelector(".guess").value);
+
+  // When there is no input
   if (!userInput) {
     message.textContent = "⛔ No Number!";
+
+    // When player wins
   } else if (userInput === secretNumber) {
     message.textContent = "🎉 Correct Number!";
     score++;
     showScore.textContent = score;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    // When user guess is too high
   } else if (userInput > secretNumber) {
     if (score > 1) {
       message.textContent = "📈 Too High!";
@@ -28,6 +35,7 @@ btnCheck.addEventListener("click", function () {
       message.textContent = "😭 You lost the game";
       showScore.textContent = 0;
     }
+    // When user guess is too low
   } else if (userInput < secretNumber) {
     if (score > 1) {
       message.textContent = "📉 Too Low!";
@@ -37,6 +45,7 @@ btnCheck.addEventListener("click", function () {
       message.textContent = "😭 You lost the game";
       showScore.textContent = 0;
     }
+    // When user enter invalid value other than 1 to 20
   } else {
     message.textContent = "❌ Invalid Input!";
   }
